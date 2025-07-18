aa4fd3f795fae3c562dcb5d75b4fdf2f065f2d79
tranvotiendo <110394763+tranvotiendo@users.noreply.github.com>
2025-07-18 19:49:03 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Avu ơiiiii!',
+    introDesc: `Anh có bất ngờ cho vk nè hehe`,
+    btnIntro: 'Iu em:))',
+    title: 'Trả lời thật đi 😙',
+    desc: 'Avu có iu ck không ',
+    btnYes: 'Cóoo, iu ck nhìu nhìu',
+    btnNo: 'Ai thèm iu a <33',
+    question:'droi, nhma tsao Avu lại iu anh nhờ <3',
+    btnReply: 'gửi cho ck <3',
+    reply: 'tai vi anh de thuong cu te pho mai que <333333',
+    mess: 'anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: '19-7-2025| Háp pi bớt đê vk iu nhó:> Chúc ebe của anh tuổi mới nhiều thành công hơn nữa, lúc nào cũng hạnh phúc nho nhoo. Anh xin lỗi vì hong đón sinh nhật năm này cùng em được. Nhưng anh mong là sau này mình sẽ có nhiều nhiều kỉ niệm đẹp với nhau nhaaa. Cám ơn vì đã có em bên cạnh. Anh iu em nhiều :3  ',
+    btnAccept: 'Mãi iu <3',
+    messLink: 'https://www.youtube.com/watch?v=bJi8SWxFJcg/' //link mess
+}
